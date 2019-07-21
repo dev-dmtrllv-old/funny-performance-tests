5015c9ab40f5243b20975fd5bff4c57ec221fb81
dev.dmtrllv <dev-dmtrllv@gmail.com>
2019-07-21 08:52:52 +0200

    updated to use better cli arguments

--- php-node-loops/run.js
+++ php-node-loops/run.js
@@ -7,26 +7,37 @@ const timerPath = resolve(__dirname, "./timers.json");
 if (existsSync(timerPath))
 	unlinkSync(timerPath);
 
-let [, , l = 10, loopCount = 1000, fullinfo = "true"] = process.argv;
+let [, , ...args] = process.argv;
+
+const getArg = (arg, def) =>
+{
+	const i = args.indexOf(arg);
+	return i > -1 ? args[i + 1] : def;
+}
+
+let tests = getArg("tests", 10);
+let loops = getArg("loops", 1000);
+let simple = args.indexOf("simple") !== -1;
+let minimal = args.indexOf("minimal") !== -1;
 
 console.clear();
-console.log(`running ${loopCount} loops ${l} times:`);
+console.log(`running ${loops} loops ${tests} times with ${simple ? "simple" : minimal ? "minimal" : "extended"} information...`);
 
-for (let i = 0; i < l; i++)
+for (let i = 0; i < tests; i++)
 {
-	execSync("C:\\php\\php-7.3.7\\php.exe ./loops.php " + loopCount);
-	execSync("node ./loops " + loopCount);
+	execSync("C:\\php\\php-7.3.7\\php.exe ./loops.php " + loops);
+	execSync("node ./loops " + loops);
 }
 
 const timers = require("./timers.json");
 
-
-const faster = timers.node.average < timers.php.average ? "node" : "php";
-const averageDiff = Math.abs(timers.node.average - timers.php.average);
 const totalDiff = Math.abs(timers.node.total - timers.php.total);
+const faster = timers.node.total < timers.php.total ? "node" : "php";
+const averageDiff = Math.abs(timers.node.average - timers.php.average);
 
-if (fullinfo == "true")
+if (!simple && !minimal)
 {
+	console.log();
 	console.log({
 		...timers, comparison: {
 			faster,
@@ -36,16 +47,16 @@ if (fullinfo == "true")
 	});
 	console.log();
 }
-else
+else if (simple && !minimal)
 {
+	console.log();
 	console.log(`node:`);
-	console.log(`  total: ${timers.node.total}`)
-	console.log(`  average: ${timers.node.average}`);
+	console.log(`  total: ${timers.node.total}ms`)
+	console.log(`  average: ${timers.node.average}ms`);
 	console.log();
 	console.log(`php:`);
-	console.log(`  total: ${timers.php.total}`);
-	console.log(`  average: ${timers.php.average}`);
+	console.log(`  total: ${timers.php.total}ms`);
+	console.log(`  average: ${timers.php.average}ms`);
 	console.log();
 }
-console.log(`${faster} is ${averageDiff} ms faster in average and ${totalDiff} ms faster in total!`);
-console.log();
+console.log(`${faster} was`, averageDiff, `ms faster in average and`, totalDiff, `ms faster in total!`);
